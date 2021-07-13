d8188280c40410ba1b41743aa40d1eba41b1dfa0
Kolomoyets473 <kolomoyets473@gmail.com>
2021-07-13 13:05:11 +0300

    v0.0.5
    === NEW ===
    1) Added Header component which is input field for  entering new todos

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import Footer from './features/footer/Footer';
+import Header from './features/header/Header';
 
 function App() {
   return (
     <div className="App">
+      
       <nav>
         <section>
           <h1>Redux Fundamentals Example</h1>
@@ -16,6 +18,7 @@ function App() {
       <section>
         <h2>Welcome to the Redux Fundamentals example app!</h2>
       </section>
+      <Header />
     <Footer />
     </div>
   )
